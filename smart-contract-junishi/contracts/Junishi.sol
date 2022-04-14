674c49479b430c0927d4dcc4a133e5b4cdc67337
vcasteline <vcasteline@ufl.edu>
2022-04-14 19:55:43 -0400

    added initial files

---
+++ smart-contract-junishi/contracts/Junishi.sol
@@ -0,0 +1,10 @@
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+string constant name = "Junishi";
+string constant symbol = "JNS";
+contract Junishi is ERC721(name, symbol){
+     
+}
